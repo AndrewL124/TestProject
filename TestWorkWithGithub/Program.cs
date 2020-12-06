fe5e2b12748408ea9e95a731a715565d78dd4ec5
AndrewL124 <startitacademy2020@gmail.com>
2020-12-06 16:35:00 +0200

    My first commit

---
+++ TestWorkWithGithub/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TestWorkWithGithub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This work! Version 1.0");
+            Console.ReadKey();
+        }
+    }
+}
